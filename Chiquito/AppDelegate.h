8f1bb772a79e84e4355ed394aab4cd9851229cf8
Miguel Martín-Nieto Morales <miguel@contxt.in>
2014-07-09 09:34:23 +0200

    Initial Commit

---
+++ Chiquito/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Chiquito
+//
+//  Created by Miguel Martin Nieto on 09/07/14.
+//  Copyright (c) 2014 ironhack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
